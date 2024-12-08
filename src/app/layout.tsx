07807c3d419e980738e8afddbe497d3f1db2ef73
shagufta-Github-Account <ik7405403@gmail.com>
2024-12-09 02:12:15 +0500

    up3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+     <link 
+        rel="stylesheet" 
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" 
+        integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" 
+        crossOrigin="anonymous" 
+        referrerPolicy="no-referrer" 
+      />
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
